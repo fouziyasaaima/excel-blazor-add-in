b9d469f0fd2d9738717a9e690ee3d08d7c796258
User <User@DESKTOP-K4F2RS8>
2023-06-21 21:38:17 +0530

    Add project files.

---
+++ excel-blazor-add-in/Pages/GetActiveSheetData.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorAddIn.Pages
+{
+    public partial class GetActiveSheetData
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = default!;
+        public IJSObjectReference JSModule { get; set; } = default!;
+
+        public string? ExcelData { get; set; }
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                JSModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/GetActiveSheetData.razor.js");
+            }
+        }
+
+        /// <summary>
+        /// Function to get Data from Excel
+        /// </summary>
+        private async Task RetrieveActiveSheetData()
+        {
+
+            ExcelData = await JSModule.InvokeAsync<String>("GetData");
+            StateHasChanged();
+       
+        }
+              
+        //Event CallBack from Javascript to Excel 
+        [JSInvokable]
+        public void GetExcelData(string data)
+        {
+            ExcelData = data;
+            StateHasChanged();
+        }
+
+    }
+
+}
+
---
+++ excel-blazor-add-in/Pages/GetActiveSheetData.razor.js
@@ -0,0 +1,22 @@
+﻿export async function GetData()
+{ 
+    var excelData;
+    var ActiveCellFormula;
+    await Excel.run(async (context) => {
+        let sheet = context.workbook.worksheets.getActiveWorksheet();
+        let range = sheet.getUsedRange();
+        let activeCell = context.workbook.getActiveCell();
+        activeCell.load("formulas");
+        range.load("values");
+        await context.sync();
+        excelData = JSON.stringify(range.values, null, 4);
+        ActiveCellFormula = JSON.stringify(activeCell.formulas, null, 4);
+        console.log(JSON.stringify(range.values, null, 4));
+    });
+
+    return "ActiveCell Formula : " + ActiveCellFormula + "Sheet Data: " + excelData ;
+   
+    
+}
+
+
